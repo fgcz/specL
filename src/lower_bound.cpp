38a0055ddeb6ae4cb89f889a54b8840745d3dbc8
Christian Panse <cp@fgcz.ethz.ch>
2015-03-03 14:27:37 +0000

    fixed Rcpp import;
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/specL@100112 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/lower_bound.cpp
+++ src/lower_bound.cpp
@@ -10,26 +10,34 @@ $Id$
 
 */
 
-RcppExport SEXP lower_bound_ (SEXP q, SEXP vec) {
-  
-  Rcpp::NumericVector xq(q);
-  Rcpp::NumericVector xvec(vec);
-  Rcpp::IntegerVector IDX; 
-  
-  try{
-  
-  for(int x : xq){
-    IDX.push_back(std::distance(xvec.begin(), 
-      std::lower_bound(xvec.begin(), xvec.end(), x, 
-        [](const double& a, const double& b){return(a <= b);})
-        )
-      );
-  }
-  return(IDX);
-  } catch(...) {
-    ::Rf_error( "c++ exception (unknown reason)" );
-  }
-  
-  // TODO(cp): handle possible exceptions
-}
+RcppExport SEXP lower_bound_(SEXP q, SEXP vec)
+{
 
+    Rcpp::NumericVector xq(q);
+    Rcpp::NumericVector xvec(vec);
+    Rcpp::IntegerVector IDX;
+
+    try {
+
+      for (int x:xq) {
+	    IDX.push_back(std::distance(xvec.begin(),
+					std::lower_bound(xvec.begin(),
+							 xvec.end(), x,
+							 [](const double
+							    &a,
+							    const double
+							    &b) {
+							 return (a <= b);
+							 }
+					)
+			  )
+		);
+	}
+	return (IDX);
+    }
+    catch( ...) {
+	::Rf_error("c++ exception (unknown reason)");
+    }
+
+    // TODO(cp): handle possible exceptions
+}
