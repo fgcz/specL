a51d7a1b35adb43c3e5a3419c6bda38d7bb2b10b
Christian Panse <cp@fgcz.ethz.ch>
2016-06-23 10:09:14 +0000

    zzz
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/specL@118893 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+#R
+
+onAttach <- function(lib, pkg){
+        if(interactive()){
+                version <- packageVersion('specL')
+                packageStartupMessage("Package 'specL' version ", version)
+          invisible()
+        }
+
+}
+
